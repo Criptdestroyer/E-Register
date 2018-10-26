38c4cc9dfc8640bca3f8672acdb824036ec13ba8
root <ahmademir1201@gmail.com>
2018-10-26 18:21:39 +0700

    template home

---
+++ application/views/home/_partials/footer.php
@@ -0,0 +1,6 @@
+<footer class="py-5 bg-black">
+      <div class="container">
+        <p class="m-0 text-center text-white small"><span>Copyright © <?php echo SITE_NAME ." ". Date('Y') ?></span></p>
+      </div>
+      <!-- /.container -->
+    </footer>
